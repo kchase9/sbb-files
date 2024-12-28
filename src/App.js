efc79f2321476f81a7b32ee14a8d3dab9b05bc01
kchase9 <kristeen.a.chase@gmail.com>
2024-12-28 01:11:20 -0400

    `Added new route for RequestDocumentChange component`

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/welcome/welcome.js';
 import Home from './pages/home/home.js';
 import AddDocument from './pages/add-document/add-document.js';
+import RequestDocumentChange from './pages/request-document-change/request-document-change.js';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/" element={<Welcome />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/add-document" element={<AddDocument />} />
+                <Route path="/change-document" element={<RequestDocumentChange />} />
                 {/* Add other routes here */}
             </Routes>
         </Router>
